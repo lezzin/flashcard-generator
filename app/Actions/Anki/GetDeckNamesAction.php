c035df5e0108e3d26dc04dd6264ec0823e170b74
Leandro <leandrinsilva22@gmail.com>
2026-03-17 14:25:03 -0300

    feat - improve deck names UX

--- app/Actions/Anki/GetDeckNamesAction.php
+++ app/Actions/Anki/GetDeckNamesAction.php
@@ -2,6 +2,8 @@
 
 namespace App\Actions\Anki;
 
+use Illuminate\Support\Str;
+
 class GetDeckNamesAction
 {
     public function __construct(
@@ -10,6 +12,31 @@ class GetDeckNamesAction
 
     public function execute()
     {
-        return $this->invokeAction->execute('deckNames');
+        $deckNames = $this->invokeAction->execute('deckNames');
+
+        return collect($deckNames)->map(function (string $deckName) {
+            $parts = explode('::', $deckName);
+            $lastIndex = count($parts) - 1;
+
+            return collect($parts)->map(function ($part, $index) use ($lastIndex) {
+                $part = trim($part);
+
+                if ($index === $lastIndex) {
+                    return $part;
+                }
+
+                if (strlen($part) <= 2) {
+                    return strtoupper($part);
+                }
+
+                if (str_contains($part, ' ')) {
+                    return collect(explode(' ', $part))
+                        ->map(fn($word) => Str::substr($word, 0, 1) . '.')
+                        ->join(' ');
+                }
+
+                return $part;
+            })->join(' > ');
+        });
     }
 }
